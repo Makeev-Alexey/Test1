900baab8fa207229916232016ee037ec8d7eb14a
aleksej <allozvr-al@mail.ru>
2022-03-23 15:54:19 +0300

    first

--- app/src/main/java/com/example/myapplication/API/Controller.kt
+++ app/src/main/java/com/example/myapplication/API/Controller.kt
@@ -2,13 +2,14 @@ package com.example.myapplication.API
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class Controller {
     companion object {
         fun getApiArguments(): RedmoappAPI {
             val retrofit = Retrofit.Builder()
                 .baseUrl("https://redmobapps.website/")
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(ScalarsConverterFactory.create())
                 .build()
             return retrofit.create(RedmoappAPI::class.java)
         }
